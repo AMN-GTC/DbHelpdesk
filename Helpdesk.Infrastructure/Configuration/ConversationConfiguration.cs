b2931b3bc9dfd5963de4deb97f206e6b096222dd
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-09 12:53:17 +0700

    Add files via upload

---
+++ Helpdesk.Infrastructure/Configuration/ConversationConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using API_DB_Conversation.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API_DB_Conversation.Configuration
+{
+    public class ConversationConfiguration : IEntityTypeConfiguration<Conversation>
+    {
+        public void Configure(EntityTypeBuilder<Conversation> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
