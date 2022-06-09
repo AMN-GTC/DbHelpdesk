c4a43dfcd7cef9a526982806852b5f0f28652457
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-09 12:52:02 +0700

    Add files via upload

---
+++ Helpdesk.Core/Repositories/IConversationRepositories.cs
@@ -0,0 +1,17 @@
+﻿using API_DB_Conversation.Entity;
+using Ardalis.Specification;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API_DB_Conversation.Repositories
+{
+    public interface IConversationRepositories
+    {
+        Task Insert(Conversation model, CancellationToken cancelationToken = default);
+        Task Delete(int id, CancellationToken cancelationToken = default);
+        Task<Conversation> GetObject(int id, CancellationToken cancelationToken = default);
+        Task<List<Conversation>> GetList(Specification<Conversation> specification, CancellationToken cancelationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
