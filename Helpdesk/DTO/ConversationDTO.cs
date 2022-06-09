c012a4b6662a8e135f73f28618692f2a4931b2e8
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-09 12:54:25 +0700

    Add files via upload

---
+++ Helpdesk/DTO/ConversationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace API_DB_Conversation.DTO
+{
+    public class ConversationDTO
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime DateTime { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
