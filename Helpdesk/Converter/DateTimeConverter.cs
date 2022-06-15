446f5bb5e24e8a7b02da7921d024afa15a7e4c66
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-15 10:21:41 +0700

    Add files via upload

--- Helpdesk/Converter/DateTimeConverter.cs
+++ Helpdesk/Converter/DateTimeConverter.cs
@@ -1,1 +1,21 @@
-date
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Globalization;
+
+namespace Helpdesk.Converter
+{
+    public class DateTimeConverter : JsonConverter<DateTime>
+    {
+        private string formatDate = "dd/MM/yyyy HH:mm";
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.ParseExact(reader.GetString(), formatDate, CultureInfo.InvariantCulture);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(formatDate));
+        }
+    }
+}
