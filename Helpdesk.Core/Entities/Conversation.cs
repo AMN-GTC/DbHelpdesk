900e263c0989ce79d9298573221ff47ad0727127
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-15 09:54:50 +0700

    Add files via upload

--- Helpdesk.Core/Entities/Conversation.cs
+++ Helpdesk.Core/Entities/Conversation.cs
@@ -1,25 +1,24 @@
 ﻿using System;
-using System.Threading.Tasks;
-using System.Linq;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace API_DB_Conversation.Entity
+namespace Helpdesk.Core.Entities
 {
-    [Table("tbl_conversation")]
-    public class Conversation
-    {
-        [Key]
-        public int Id { get; set; }
-        public int TicketId { get; set; }
-        public string ToEmail { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
-        public DateTime DateTime { get; set; }
-        public string CreatedBy { get; set; }
+        [Table("tbl_conversation")]
+        public class Conversation
+        {
+            [Key]
+            public int Id { get; set; }
+            public int TicketId { get; set; }
+            public string ToEmail { get; set; }
+            public string Subject { get; set; }
+            public string Body { get; set; }
+            public DateTime DateTime { get; set; }
+            public string CreatedBy { get; set; }
 
+        }
     }
-    
-
-}
