413bc24da6443c12957c70edc6255bd35d3004cc
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-15 09:55:34 +0700

    Add files via upload

--- Helpdesk.Core/Repositories/IConversationRepositories.cs
+++ Helpdesk.Core/Repositories/IConversationRepositories.cs
@@ -1,10 +1,13 @@
-﻿using API_DB_Conversation.Entity;
-using Ardalis.Specification;
+﻿using Ardalis.Specification;
+using Helpdesk.Core.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace API_DB_Conversation.Repositories
+namespace Helpdesk.Core.Repositories
 {
     public interface IConversationRepositories
     {
