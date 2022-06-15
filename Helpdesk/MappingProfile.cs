a1699882fb957859d5345c617541bf12863b6135
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-15 09:54:01 +0700

    Add files via upload

--- Helpdesk/MappingProfile.cs
+++ Helpdesk/MappingProfile.cs
@@ -8,9 +8,11 @@ namespace Helpdesk
     {
         public MappingProfile()
         {
+            base.CreateMap<VwLastWeekTicket, VwLastWeekTicketDTO>().ReverseMap();
             base.CreateMap<VwTicketSummary, VwTicketSummaryDTO>().ReverseMap();
             base.CreateMap<VwTicketPIC, VwTicketPICDTO>().ReverseMap();
             base.CreateMap<VwActiveTicketSummary, VwActiveTicketSummaryDTO>().ReverseMap();
+            base.CreateMap<Conversation, ConversationDTO>().ReverseMap();
         }
     }
 }
