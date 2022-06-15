95d361b2927842eaf7c0f993628a4521cc411e1c
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-15 10:17:53 +0700

    Add files via upload

---
+++ Helpdesk.Infrastructure/Repositories/ConversationRepositories.cs
@@ -0,0 +1,56 @@
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+using Helpdesk.Core.Entities;
+using Helpdesk.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Helpdesk.Infrastructure.Repositories
+{
+    public class ConversationRepositories : IConversationRepositories
+    {
+        private readonly DbContext _dbContext;
+        protected readonly DbSet<Conversation> _dbSetConversation;
+        public ConversationRepositories(DbContext context)
+        {
+            _dbContext = context;
+            _dbSetConversation = _dbContext.Set<Conversation>();
+        }
+
+        public Task<List<Conversation>> GetList(Specification<Conversation> specification, CancellationToken cancelationToken = default)
+        {
+            IQueryable<Conversation> query = _dbSetConversation.AsQueryable();
+            SpecificationEvaluator evaluator = new SpecificationEvaluator();
+            query = evaluator.GetQuery(query, specification);
+            return query.ToListAsync(cancelationToken);
+        }
+
+        public async Task<Conversation> GetObject(int id, CancellationToken cancelationToken = default)
+        {
+            return await _dbSetConversation.FirstOrDefaultAsync(f => f.Id == id, cancelationToken);
+        }
+
+        public async Task Insert(Conversation model, CancellationToken cancelationToken = default)
+        {
+            await _dbSetConversation.AddAsync(model, cancelationToken);
+        }
+        public async Task Delete(int id, CancellationToken cancelationToken = default)
+        {
+            Conversation existing = await _dbSetConversation.FindAsync(new object[] { id }, cancelationToken);
+            if (existing == null)
+            {
+                throw new Exception($"email dengan id = {id} tidak ditemukan");
+            }
+            _dbContext.Remove(existing);
+        }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
