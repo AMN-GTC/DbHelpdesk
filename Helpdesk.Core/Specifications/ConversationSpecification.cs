5f8f1c04743bc2e0c689b6a1a8ee35a9bcaaf299
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-15 10:15:21 +0700

    Add files via upload

--- Helpdesk.Core/Specifications/ConversationSpecification.cs
+++ Helpdesk.Core/Specifications/ConversationSpecification.cs
@@ -1,1 +1,23 @@
-spec
+﻿using Ardalis.Specification;
+using Helpdesk.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpdesk.Core.Specifications
+{
+    public class ConversationSpecification : Specification<Conversation>
+    {
+        public Nullable<int> Idequal { get; set; }
+        public Specification<Conversation> Build()
+        {
+            if (Idequal.HasValue)
+            {
+                Query.Where(f => f.Id == Idequal.Value);
+            }
+            return this;
+        }
+    }
+}
