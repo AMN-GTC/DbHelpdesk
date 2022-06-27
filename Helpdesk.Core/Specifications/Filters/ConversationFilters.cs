75c6587a9a83691e8e8a17fa8bde3f813389d391
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-27 16:00:19 +0700

    Create ConversationFilters.cs

---
+++ Helpdesk.Core/Specifications/Filters/ConversationFilters.cs
@@ -0,0 +1,29 @@
+using API_DB_Ticket.Entity;
+using Ardalis.Specification;
+using System.Collections.Generic;
+
+namespace API_DB_Ticket.Specification.Filters
+{
+    public class ConversationFilter
+    {
+        public ConversationFilter(Dictionary<string, string> param)
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            foreach (var pair in param)
+            {
+                if (!keyValuePairs.ContainsKey(pair.Key.ToLower()))
+                {
+                    keyValuePairs.Add(pair.Key.ToLower(), pair.Value);
+                }
+                keyValuePairs[pair.Key.ToLower()] = pair.Value;
+            }
+
+        }
+        public Specification<Conversation> ToSpecification()
+        {
+           ConversationSpecification conversationSpecification = new ConversationSpecification();
+          
+            return conversationSpecification.Build();
+        }
+    }
+}
