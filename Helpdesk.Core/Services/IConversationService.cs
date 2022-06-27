985759c199dc130dd49dbd323ab8e4b1c408b38c
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-06-27 16:01:30 +0700

    Update IConversationService.cs

--- Helpdesk.Core/Services/IConversationService.cs
+++ Helpdesk.Core/Services/IConversationService.cs
@@ -1,22 +1,28 @@
-﻿using Ardalis.Specification;
-using Helpdesk.Core.Entities;
+using API_DB_Ticket.Entity;
+using API_DB_Ticket.Settings;
+using Ardalis.Specification;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Helpdesk.Core.Services
+namespace API_DB_Ticket.Services
 {
     public interface IConversationService : IService
     {
+        
+       
         public Task SendEmailAsync(Conversation conversation, CancellationToken cancellationToken = default);
         public Task<Conversation> Insert(Conversation conversation, CancellationToken CancellationToken = default);
         public Task<bool> Delete(int id, CancellationToken cancellationToken = default);
         public Task<Conversation> GetObject(int id, CancellationToken cancellationToken = default);
         public Task<List<Conversation>> GetList(Specification<Conversation> specification, CancellationToken cancellationToken = default);
-        //public Task<bool> GetConversationFromEmail(CancellationToken cancellationToken = default);
-        //public Task<IReadOnlyList<Conversation>> GetConversations(ISpecification<Conversation> filter, CancellationToken cancellationToken = default);
+        public Task<Conversation> SendConversation(Conversation conversation, CancellationToken cancellationToken = default);
+        public EmailStack BindStackEmailToConversation(Email email);
+        public Task<bool> GetConversationFromEmail(CancellationToken cancellationToken = default);
+        public Task<IReadOnlyList<Conversation>> GetConversations(Specification<Conversation> filter);
+
+
+
     }
 }
