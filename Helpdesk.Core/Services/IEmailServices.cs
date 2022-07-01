d51ccea735c920223be7bda510e6525c1786a43d
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-07-01 16:54:20 +0700

    Add files via upload

---
+++ Helpdesk.Core/Services/IEmailServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Ardalis.Specification;
+using Helpdesk.Core.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpdesk.Core.Common.Mailer;
+
+namespace Helpdesk.Core.Services
+{
+    public interface IServiceEmail
+    {
+        Dictionary<string, List<string>> GetError();
+
+        Boolean GetServiceState();
+    }
+    public interface IEmailServices : IServiceEmail
+    {
+        Task<Email> Insert(Email emailStack, CancellationToken cancellationToken = default);
+        Task<bool> Update(Email emailStack, string id, CancellationToken cancellationToken = default);
+        Task<bool> Delete(string id, CancellationToken cancellationToken);
+        Task<List<Email>> GetList(Specification<Email> specification, CancellationToken cancellationToken = default);
+        Task<Email> GetObject(Email emailStack, string id, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ Helpdesk.Core/Services/IEmailStackService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+using Helpdesk.Core.Common.Mailer;
+using Helpdesk.Core.Entities;
+
+namespace Helpdesk.Core.Services
+{
+    public interface IServiceEmailStack
+    {
+        Dictionary<string, List<string>> GetError();
+
+        Boolean GetServiceState();
+    }
+    public interface IEmailStackService : IServiceEmailStack
+    {
+        public Task<EmailStack> Insert(EmailStack email, CancellationToken cancellationToken = default);
+
+        public Task<EmailStack> Update(int id, EmailStack emailStack, CancellationToken cancellationToken = default);
+
+        public Task<EmailStack> GetObject(int id, CancellationToken cancellationToken = default);
+
+        public Task<List<EmailStack>> GetList(Specification<EmailStack> specification, CancellationToken cancellationToken = default);
+
+        public Task <bool> GetUnreadEmail (CancellationToken cancellationToken = default);
+        
+        List<EmailStack> ConvertListEmailToListEmailStack(List<Email> mails);
+
+        List<Ticket> TicketToEmail(List<EmailStack> emailStack);
+
+        Ticket TicketToEmail2(EmailStack emailStack);
+        public Task<bool> TickertMaker(EmailStack emailStack,Project projectInfo, CancellationToken cancellationToken = default);
+
+        public Task<bool> TicketMakers(List <EmailStack> emailStacks, CancellationToken cancellationToken = default);
+        }
+    }
