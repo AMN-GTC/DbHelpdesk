d50da41546c3d7f85df43d3fe7dace0390709ebe
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-07-01 17:00:06 +0700

    Add files via upload

---
+++ Helpdesk.Infrastructure/Configuration/EmailStackConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Helpdesk.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Helpdesk.Infrastructure.Configuration
+{
+    public class EmailStackConfiguration : IEntityTypeConfiguration<EmailStack>
+    {
+        public void Configure(EntityTypeBuilder<EmailStack> builder)
+        {
+            builder.HasKey(f => f.Id);
+            builder.Property(f => f.Id)
+                .ValueGeneratedOnAdd();
+
+        }
+
+    }
+}
---
+++ Helpdesk.Infrastructure/Configuration/MailConfig.cs
@@ -0,0 +1,22 @@
+﻿using Helpdesk.Core.Entities;
+using Helpdesk.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Helpdesk.Core.Common.Mailer;
+
+namespace Helpdesk.Infrastructure.Configuration
+{
+    public class MailConfig : IEntityTypeConfiguration<Email>
+    {
+        public void Configure(EntityTypeBuilder<Email> builder)
+        {
+            builder.HasKey(f => f.MsgID);
+            builder.Property(f => f.MsgID);
+            
+        } 
+        
+    }
+}
