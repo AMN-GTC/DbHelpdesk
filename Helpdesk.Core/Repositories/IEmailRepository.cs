087eb583a5578789d37e4c63bceb7dbd28257931
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-07-01 16:49:55 +0700

    Add files via upload

---
+++ Helpdesk.Core/Repositories/IEmailRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ardalis.Specification;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Helpdesk.Core.Common.Mailer;
+
+namespace Helpdesk.Core.Repositories
+{
+    public interface IEmailRepository
+    {
+        Task Insert (Email model, CancellationToken cancellationToken = default);
+
+        Task Update(Email model, string id , CancellationToken cancellationToken = default);
+        
+        Task Delete(string id, CancellationToken cancellationToken = default);
+        
+        Task<Email> GetObject(string id /*int id*/, CancellationToken cancellationToken = default);
+        
+        Task<List<Email>> GetList(Specification<Email> filter, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ Helpdesk.Core/Repositories/IEmailStackRepository.cs
@@ -0,0 +1,24 @@
+﻿using Ardalis.Specification;
+using Helpdesk.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Helpdesk.Core.Repositories
+{
+    public interface IEmailStackRepository
+    {
+        Task Insert(EmailStack model, CancellationToken cancellationToken = default);
+
+        Task Update(EmailStack model, int id, CancellationToken cancellationToken = default);
+
+        Task Delete(int id, CancellationToken cancellationToken = default);
+
+        Task<EmailStack> GetObject(int id, CancellationToken cancellationToken = default);
+
+        Task<List<EmailStack>> GetList(Specification<EmailStack> specification, CancellationToken cancellationToken = default);
+    }
+}
