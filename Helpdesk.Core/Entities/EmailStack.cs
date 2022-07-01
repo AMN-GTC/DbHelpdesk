5c31998df9da53555864322718cef125f4ddd1f5
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-07-01 16:47:18 +0700

    Add files via upload

---
+++ Helpdesk.Core/Entities/EmailStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Helpdesk.Core.Entities
+{
+    
+    public class EmailStack
+        
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsProcessed { get; set; }
+        public int ProjectId { get; set; }
+        public string FromStack { get; set; }
+        public string ToStack  { get; set; }
+        public string SubjectStack { get; set; }
+        public string HtmlAsBodyStack { get; set; }
+        public string BodyStack { get; set; }
+        public DateTime MailDateTimeStack { get; set; }
+        public string MsgIDStack { get; set; }
+        public string MsgThreadIDStack { get; set; }
+        public DateTime MailDateReStack { get; set; }
+    }
+}
