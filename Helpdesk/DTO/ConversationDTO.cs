a2dd32a101efddeff20cdc255c70dda9d6f8e200
syahrulmp <71216509+syahrulmp@users.noreply.github.com>
2022-07-01 17:07:08 +0700

    Add files via upload

--- Helpdesk/DTO/ConversationDTO.cs
+++ Helpdesk/DTO/ConversationDTO.cs
@@ -6,10 +6,12 @@ namespace Helpdesk.DTO
     {
         public int Id { get; set; }
         public int TicketId { get; set; }
+        public int ProjectId { get; set; }
         public string ToEmail { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
         public DateTime DateTime { get; set; }
         public string CreatedBy { get; set; }
+        public string Password { get; set; }
     }
 }
---
+++ Helpdesk/DTO/EmailStackDTO.cs
@@ -0,0 +1,19 @@
+﻿using Helpdesk.Core.Entities;
+using System;
+
+namespace Helpdesk
+{
+    public class EmailStackDTO
+    {
+        public int Id { get; set; }
+        public bool IsProcessed { get; set; }
+        public string FromStack { get; set; }
+        public string SubjectStack { get; set; }
+        public string HtmlAsBodyStack { get; set; }
+        public string BodyStack { get; set; }
+        public DateTime MailDateTimeStack { get; set; }
+        public string MsgIDStack { get; set; }
+        public string MsgThreadIDStack { get; set; }
+        public DateTime MailDateReStack {   get; set; }
+    }
+}
