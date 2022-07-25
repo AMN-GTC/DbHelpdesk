1d29f605040045e65a959ed1d44ac811646a58dc
fareesaa <97422732+fareesaa@users.noreply.github.com>
2022-07-25 13:34:28 +0700

    Update EmailStackDTO.cs

--- Helpdesk/DTO/EmailStackDTO.cs
+++ Helpdesk/DTO/EmailStackDTO.cs
@@ -7,13 +7,15 @@ namespace Helpdesk
     {
         public int Id { get; set; }
         public bool IsProcessed { get; set; }
+        public int ProjectId { get; set; }
         public string FromStack { get; set; }
+        public string ToStack { get; set; }
         public string SubjectStack { get; set; }
         public string HtmlAsBodyStack { get; set; }
         public string BodyStack { get; set; }
         public DateTime MailDateTimeStack { get; set; }
         public string MsgIDStack { get; set; }
         public string MsgThreadIDStack { get; set; }
-        public DateTime MailDateReStack {   get; set; }
+        public DateTime MailDateReStack { get; set; }
     }
 }
